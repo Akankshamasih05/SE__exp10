909fe0c6f8002a9fec28e2e9861c309e3d9e7f61
Akankshamasih05 <Akankshamasih05@users.noreply.github.com>
2021-10-27 13:09:54 +0530

    third commit

--- prob.java
+++ prob.java
@@ -1,5 +1,7 @@
 class Simple{
     public static void main(String args[]){
      System.out.println("Hello Java");
+     String name = "Akanksha";
+            System.out.println(name);
     }
-}  
+}
